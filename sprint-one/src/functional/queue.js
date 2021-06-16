81041b271b9850da54cd4e22175cc8e5560bd59b
Gulina Tayier <gitayier84@gamil.com>
2021-06-16 16:28:46 -0700

    Almost finished sprint one functional part

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,37 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
 
   // Implement the methods below
+  //first in first out,
+  //in from the back door which is last index
+  // out from the front door which is first index
 
   someInstance.enqueue = function(value) {
+    //enqueue(string) - Add a string to the back of the queue
+    var indexToBe = Object.keys(someInstance.storage).length;
+    someInstance.storage[indexToBe] = value;
   };
 
   someInstance.dequeue = function() {
+    //dequeue() - Remove and return the string at the front of the queue
+    var dequeuedItem = someInstance.storage[0];
+    delete someInstance.storage[0];
+    for (var i = 0; i <= someInstance.storage.length; i++) {
+      someInstance.storage[i] = someInstance.storage[ i + 1];
+    }
+
+    return dequeuedItem;
   };
 
+
+
   someInstance.size = function() {
+    return Object.keys(someInstance.storage).length;
   };
 
   return someInstance;
 };
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
+  var num = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.storage[num] = value;
+    num++;
   };
 
   someInstance.pop = function() {
+    var IndexOflastIn = Object.keys(someInstance.storage).length - 1;
+    var popedItem = someInstance.storage[IndexOflastIn];
+    delete someInstance.storage[IndexOflastIn];
+    num--;
+    return popedItem;
   };
 
   someInstance.size = function() {
+    return Object.keys(someInstance.storage).length;
   };
 
   return someInstance;
 };
+
