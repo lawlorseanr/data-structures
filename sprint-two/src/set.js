9ea11be62b9deffbfa6c490cb5f83b2309b160f6
Sean Lawlor <lawlorseanr@gmail.com>
2021-06-17 16:31:02 -0700

    Pseudocode Set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,44 @@
 var Set = function() {
   var set = Object.create(setPrototype);
+
+  // set the _storage value to an empty array
   set._storage = null; // fix me
   return set;
 };
 
 var setPrototype = {};
 
+// takes any string and adds it to the set
 setPrototype.add = function(item) {
+  /*
+    add the item to the end of hte array
+  */
 };
 
+
+// method, takes any string and returns a boolean
+// reflecting whether it can be found in the set
 setPrototype.contains = function(item) {
+  /*
+    iterate through the array
+      if the current index equals the item
+        found it, return true
+
+    if we get to thsi point, it isn't in the array, return false
+  */
 };
 
+// method, takes any string and removes it from the set, if present
 setPrototype.remove = function(item) {
+  /*
+    iterate through the array
+      if the current index equals the item
+        if not the very last index of the array
+          set current index equal to next index
+
+        if the very last index of the array
+          remove the last index of the array
+  */
 };
 
 /*
