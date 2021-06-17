fb049e01f722592a61284f0d88b7fcfc78abd3fe
Gulina Tayier <gitayier84@gamil.com>
2021-06-17 09:02:39 -0700

    finished pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // use new and this keyword and capitalize the class name
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  var indexToBe = Object.keys(this.storage).length;
+  this.storage[indexToBe] = value;
+};
+Queue.prototype.dequeue = function() {
+  var dequeuedItem = this.storage[0]; //0:a, 1:b, 2:c
+  delete this.storage[0]; // 1:b, 2:c
+  for (var i = 0; i < Object.keys(this.storage).length; i++) {
+    this.storage[i] = this.storage[i + 1]; //0:b, 1:c, 2:c
+  }
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return dequeuedItem;
+};
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
+var queue = new Queue;
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // use new and this keyword and capitalize the class name
+  this.storage = {};
 };
 
+Stack.prototype.push = function (value) {
+  var indexToBe = Object.keys(this.storage).length;
+  this.storage[indexToBe] = value;
+};
+Stack.prototype.pop = function() {
+  var popedItem = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return popedItem;
+};
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
+var stack = new Stack;
