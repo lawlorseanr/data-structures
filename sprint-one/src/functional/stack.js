a3626890903da26ef3184cf52c863f73c0ebd054
Gulina Tayier <gitayier84@gamil.com>
2021-06-16 22:31:10 -0700

    updated stack in functional part

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,14 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
   someInstance.storage = storage;
   var num = 0;
-
   // Implement the methods below
   someInstance.push = function(value) {
     someInstance.storage[num] = value;
     num++;
   };
-
   someInstance.pop = function() {
     var IndexOflastIn = Object.keys(someInstance.storage).length - 1;
     var popedItem = someInstance.storage[IndexOflastIn];
@@ -19,11 +16,9 @@ var Stack = function() {
     num--;
     return popedItem;
   };
-
   someInstance.size = function() {
     return Object.keys(someInstance.storage).length;
   };
-
   return someInstance;
 };
 
