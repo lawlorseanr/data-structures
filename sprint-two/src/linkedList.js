df61ac6a578ee8d63d1b87d3bd631982a43eb934
Gulina Tayier <gitayier84@gamil.com>
2021-06-17 21:50:44 -0700

    Finished LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //if empty, means head and tail is null
+    // change head pointer to node pass in value, same for tail
+    if (list.head === null && list.tail === null) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    } else {
+      list.tail = Node(value);
+      list.head.next = Node(value);
+      list.tail.next = null;
+    }
   };
 
   list.removeHead = function() {
+    var removedNodeValue = list.head.value;
+    list.head = list.head.next;
+    return removedNodeValue;
+
   };
 
   list.contains = function(target) {
+    var recur = function(currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      if (currentNode.next !== null) {
+        return recur(currentNode.next);
+      }
+      return false;
+    };
+    return recur(list.head);
   };
 
   return list;
@@ -26,4 +50,8 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail: O(1);
+ removeHead: O(1);
+ contains: O(n);
+
  */
