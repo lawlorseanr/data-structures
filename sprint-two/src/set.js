d2df6be22efd924963d9a041ae254e401f42bf57
Gulina Tayier <gitayier84@gamil.com>
2021-06-18 15:36:59 -0700

    Added .has() and .size for set to reinforce my understanding of set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -24,6 +24,20 @@ setPrototype.remove = function(item) {
   delete this._storage[item];
 };
 
+setPrototype.has = function (item) {
+  if (this._storage.length !== 0) {
+    for (var keys in this._storage) {
+      if (this._storage[keys] === item) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+setPrototype.size = function () {
+  return Object.keys(this._storage).length;
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
