c4000021440c0cc16fb6c1cbad498dacfc2bee05
Gulina Tayier <gitayier84@gamil.com>
2021-06-18 14:32:45 -0700

    Finished set but haven't added more test for set in spec.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
+//set is an Object.keys[] gather all the keys in a
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  for (var keys in this._storage) {
+    if (keys === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
